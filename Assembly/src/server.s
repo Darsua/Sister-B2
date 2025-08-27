9fdb66544b441daf5584896a218221bbf8a0472f
Darsua <darrelyanuar@gmail.com>
2025-08-27 10:55:59 +0700

    feat: method handling

--- Assembly/src/server.s
+++ Assembly/src/server.s
@@ -1,9 +1,9 @@
 .intel_syntax noprefix
 
 # -----------------------------------------------------------
-
 .section .data
-# System call numbers
+
+# -- System call numbers --
 SYS_socket = 41
 SYS_bind = 49
 SYS_listen = 50
@@ -17,37 +17,46 @@ SYS_close = 3
 SYS_fork = 57
 SYS_exit = 60
 
-# Socket constants
+# -- Socket constants --
 AF_INET=2
 SOCK_STREAM=1
 
 # Sockaddr struct
 sockaddr:
     .word AF_INET
-    .byte 0x1b, 0x39 # Port (6969)
+    .byte 0x1e, 0x61 # Port (7777)
     .byte 0, 0, 0, 0 # IP
     .zero 8 # Padding to make it 16 bytes
     
-# Web constants
-path: .asciz "res/index.html"
+# -- Headers --
 http_header: .asciz "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n"
 http_header_len = . - http_header
 
-# Debug messages
-forked_msg: .asciz "Forked\n"
-forked_msg_len = . - forked_msg
+method_not_allowed_header: .asciz "HTTP/1.1 405 Method Not Allowed\r\nContent-Type: text/html\r\n\r\n"
+method_not_allowed_header_len = . - method_not_allowed_header
+
+file_not_found_header: .asciz "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n"
+file_not_found_header_len = . - file_not_found_header
+
+internal_server_error_header: .asciz "HTTP/1.1 500 Internal Server Error\r\nContent-Type: text/html\r\n\r\n"
+internal_server_error_header_len = . - internal_server_error_header
+
 
 # -----------------------------------------------------------
-
 .section .bss
+
 socket: .quad 0 # Host socket file descriptor
 client: .quad 0 # Client socket file descriptor
 buffer_recv: .space 1024 # Buffer for reading requests
 buffer_send: .space 1024 # Buffer for sending responses
+request_path: .space 256 # Buffer for storing the requested path
+full_path: .space 260 # Buffer for storing full file path
+
 
 # -----------------------------------------------------------
-
 .section .text
+
+# -- Create server socket and listen for connections --
 .global _start
 _start:
     # Create socket
@@ -71,6 +80,8 @@ _start:
     mov rsi, 5 # Backlog
     syscall ## listen(socket, 5)
 
+    
+# -- Loop to accept and handle connections --
 accept_loop:
     # Accept connection
     mov rax, SYS_accept
@@ -94,64 +105,185 @@ accept_loop:
     syscall ## close(client_socket)
     jmp accept_loop
 
-handle_client:
-
-    # DEBUG: PRINT FORKED
-    mov rax, SYS_write
-    mov rdi, 1 # stdout
-    lea rsi, forked_msg
-    mov rdx, forked_msg_len
-    syscall ## write(1, "Forked\n", 7)
     
+# -- Handle client request in child process --
+handle_client:
     # Read request from client
     mov rax, SYS_read
     mov rdi, client # Client socket fd
     lea rsi, buffer_recv # Buffer to read into
     mov rdx, 1024 # Number of bytes to read
     syscall ## read(client_socket, buffer, 1024)
+    mov r15, rax # Save number of bytes read
     
-    # Open requested file
-    # Assuming the request is for "res/index.html"
+    # Print the request to server console (stdout)
+    mov rax, SYS_write
+    mov rdi, 1 # stdout
+    lea rsi, buffer_recv
+    mov rdx, r15 # Number of bytes read from client
+    syscall ## write(1, buffer, bytes_read)
+
+    
+# -- Parse HTTP method --
+parse_method:
+    lea rsi, buffer_recv
+    mov eax, [rsi] # Load first 4 bytes
+
+    cmp eax, 0x20544547 # "GET " in little-endian
+    je handle_get
+
+    cmp eax, 0x54534F50   # "POST" in little-endian
+    je handle_post
+    
+    cmp eax, 0x20545550   # "PUT " in little-endian
+    je handle_put
+    
+    cmp eax, 0x454C4544   # "DELE" in little-endian
+    jne method_not_supported
+    mov ebx, [rsi+4]
+    cmp ebx, 0x20204554   # "TE  " in little-endian
+    je handle_delete
+
+    
+# -- Parse request path --
+parse_path: # rsi should point to buffer_recv before calling
+    mov rcx, 4 # Start after method and space
+    lea rdi, request_path
+
+parse_path_loop:
+    mov al, [rsi + rcx]
+    cmp al, ' ' # End of path
+    je path_done
+    mov [rdi], al
+    inc rdi
+    inc rcx
+    jmp parse_path_loop
+
+path_done:
+    mov BYTE PTR [rdi], 0 # Null-terminate
+
+    # Build full path: "res/" + request_path (skip leading '/')
+    lea rsi, full_path
+    mov BYTE PTR [rsi+0], 'r'
+    mov BYTE PTR [rsi+1], 'e'
+    mov BYTE PTR [rsi+2], 's'
+    mov BYTE PTR [rsi+3], '/'
+    lea rdi, request_path
+    mov rcx, 0
+
+copy_path:
+    mov al, [rdi+rcx+1] # Skip leading '/'
+    mov [rsi+4+rcx], al
+    cmp al, 0
+    je path_built
+    inc rcx
+    jmp copy_path
+
+path_built:
+    ret
+
+    
+# -- Header response handler --
+method_not_supported:
+    # Send 405 Method Not Allowed
+    mov rax, SYS_write
+    mov rdi, client
+    lea rsi, method_not_allowed_header
+    mov rdx, method_not_allowed_header_len
+    syscall ## write(client, method_not_allowed_header, method_not_allowed_header_len)
+    jmp close_client
+
+file_not_found:
+    # Send 404 Not Found
+    mov rax, SYS_write
+    mov rdi, client
+    lea rsi, file_not_found_header
+    mov rdx, file_not_found_header_len
+    syscall ## write(client, file_not_found_header, file_not_found_header_len)
+    jmp close_client
+    
+internal_server_error:
+    # Send 500 Internal Server Error
+    mov rax, SYS_write
+    mov rdi, client
+    lea rsi, internal_server_error_header
+    mov rdx, internal_server_error_header_len
+    syscall ## write(client, internal_server_error_header, internal_server_error_header_len)
+    jmp close_client
+    
+    
+# --- MEHTHOD HANDLERS ---
+# -- GET --
+handle_get:
+    lea rsi, buffer_recv
+    call parse_path
+
+    # Open file for reading
     mov rax, SYS_open
-    lea rdi, path # Path to the file
+    lea rdi, full_path
     xor rsi, rsi # O_RDONLY
-    xor rdx, rdx # No flags
-    syscall ## open("res/index.html", O_RDONLY)
-    mov r14, rax # Save file descriptor in r14
-    
-    # Read file content
+    xor rdx, rdx
+    syscall ## open(full_path, O_RDONLY)
+    cmp rax, 0
+    jl file_not_found
+    mov r14, rax
+
+    # Read file
     mov rax, SYS_read
-    mov rdi, r14 # File descriptor
-    lea rsi, buffer_send # Buffer to read into
-    mov rdx, 1024 # Number of bytes to read
-    syscall ## read(file_descriptor, buffer_send, 1024)
-    mov r15, rax # Save number of bytes read in r15
-    
+    mov rdi, r14
+    lea rsi, buffer_send
+    mov rdx, 1024
+    syscall ## read(file_fd, buffer_send, 1024)
+    mov r15, rax
+
     # Write HTTP header
     mov rax, SYS_write
-    mov rdi, client # Client socket fd
-    lea rsi, http_header # Buffer with HTTP header
-    mov rdx, http_header_len # Length of HTTP header
-    syscall ## write(client_socket, http_header, http_header_len)
-    
-    # Write page content
-    mov rax, SYS_write
-    mov rdi, client # Client socket fd
-    lea rsi, buffer_send # Buffer with file content
-    mov rdx, r15 # Number of bytes read from file
-    syscall ## write(client_socket, buffer_send, bytes_read)
+    mov rdi, client
+    lea rsi, http_header
+    mov rdx, http_header_len
+    syscall ## write(client, http_header, http_header_len)
 
-    # Close file descriptor
-    mov rax, SYS_close
-    mov rdi, r14 # File descriptor
-    syscall ## close(file_descriptor)
+    # Write file content
+    mov rax, SYS_write
+    mov rdi, client
+    lea rsi, buffer_send
+    mov rdx, r15
+    syscall ## write(client, buffer_send, bytes_read)
     
+    # Close file
+    mov rax, SYS_close
+    mov rdi, r14
+    syscall ## close(file_fd)
+    jmp close_client
+
+    
+# -- POST --
+handle_post:
+    lea rsi, buffer_recv
+    call parse_path
+    # Continue with POST logic
+
+# -- PUT --
+handle_put:
+    lea rsi, buffer_recv
+    call parse_path
+    # Continue with PUT logic
+
+    
+# -- DELETE --
+handle_delete:
+    lea rsi, buffer_recv
+    call parse_path
+    # Continue with DELETE logic
+
+# -- Finish client request --    
+close_client:
     # Close client socket
     mov rax, SYS_close
-    mov rdi, client # Client socket fd
-    syscall ## close(client_socket)
+    mov rdi, client
+    syscall
     
-    # Child process exits
+    # Exit child process
     mov rax, SYS_exit
     xor rdi, rdi
-    syscall ## exit(0)
+    syscall
